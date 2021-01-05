1cb50a12ff78f1d717df5cbd26f735e3b3282aa3
김상현 <llmm030506@naver.com>
2021-01-05 11:52:28 +0900

    AuthDomain modeling

---
+++ src/main/java/com/gsm/jupjup/domain/AuthDomain.java
@@ -0,0 +1,43 @@
+package com.gsm.jupjup.domain;
+
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AuthDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long auth_Idx;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(length = 4, nullable = false)
+    private String classNumber;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @CreatedDate
+    private LocalDateTime create_at;
+
+    @Builder
+    public AuthDomain(String email, String classNumber, String password, String name){
+        this.email = email;
+        this.classNumber = classNumber;
+        this.password = password;
+        this.name = name;
+    }
+}
