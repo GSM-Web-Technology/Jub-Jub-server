92ce6b4a2f546ef389c2b3d65a3bb606edb914d7
siwony_ <siwon103305@gmail.com>
2021-01-12 23:17:52 +0900

    [add] EquipmentService do save logic

--- src/main/java/com/gsm/jupjup/service/EquipmentService.java
+++ src/main/java/com/gsm/jupjup/service/EquipmentService.java
@@ -1,2 +1,25 @@
-package com.gsm.jupjup.service;public class EquipmentService {
+package com.gsm.jupjup.service;
+
+import com.gsm.jupjup.dao.EquipmentRepository;
+import com.gsm.jupjup.domain.EquipmentDomain;
+import com.gsm.jupjup.dto.Equipment.EquipmentRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class EquipmentService {
+    @Autowired
+    EquipmentRepository equipmentRepo;
+
+    @Transactional
+    public Long save(EquipmentRequestDto equipmentRequestDto){
+        return equipmentRepo.save(equipmentRequestDto.toEntity()).getEq_Idx();
+    }
+
+//    @Transactional
+//    public Long update(Long eq_idx, EquipmentRequestDto equipmentRequestDto){
+//        return
+//    };
 }
