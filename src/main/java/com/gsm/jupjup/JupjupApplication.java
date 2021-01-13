5a10c5c8e2ddd30058c75cb32d4745a6f63c17e6
siwony_ <siwon103305@gmail.com>
2021-01-13 16:55:39 +0900

    [add]annotation @EnableJpaAuditiong

--- src/main/java/com/gsm/jupjup/JupjupApplication.java
+++ src/main/java/com/gsm/jupjup/JupjupApplication.java
@@ -4,10 +4,12 @@ import com.gsm.jupjup.domain.AuthDomain;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JupjupApplication {
 
