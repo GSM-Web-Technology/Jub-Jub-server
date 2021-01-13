407457cb83888b407e9a1c1b21bf291eeb52eeac
siwony_ <siwon103305@gmail.com>
2021-01-13 17:04:21 +0900

    [update]implements Read Equipment

--- src/main/java/com/gsm/jupjup/service/EquipmentService.java
+++ src/main/java/com/gsm/jupjup/service/EquipmentService.java
@@ -3,6 +3,7 @@ package com.gsm.jupjup.service;
 import com.gsm.jupjup.dao.EquipmentRepository;
 import com.gsm.jupjup.domain.EquipmentDomain;
 import com.gsm.jupjup.dto.Equipment.EquipmentRequestDto;
+import com.gsm.jupjup.dto.Equipment.EquipmentResponseDto;
 import com.gsm.jupjup.dto.test.TestResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,15 +22,20 @@ public class EquipmentService {
 
     @Transactional
     public Long update(Long eq_idx, EquipmentRequestDto equipmentRequestDto){
-        EquipmentDomain equipmentDomain = equipmentRepo.findById(eq_idx).orElseThrow(() -> new IllegalArgumentException("해당 장비는 없습니다. eq_idx="+eq_idx));
+        EquipmentDomain equipmentDomain = equipmentRepo.findById(eq_idx).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. eq_idx="+eq_idx));
         equipmentDomain.update(equipmentRequestDto.getName(), equipmentRequestDto.getImg_equipment(), equipmentRequestDto.getContent(), equipmentRequestDto.getCount());
         return eq_idx;
     };
 
     @Transactional
     public void delete(Long eq_idx){
-        EquipmentDomain equipmentDomain = equipmentRepo.findById(eq_idx).orElseThrow(() -> new IllegalArgumentException("해당 장비는 없습니다. id="+eq_idx));
+        EquipmentDomain equipmentDomain = equipmentRepo.findById(eq_idx).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. eq_idx="+eq_idx));
         equipmentRepo.delete(equipmentDomain);
     }
 
+    @Transactional(readOnly = true)
+    public EquipmentResponseDto findById(Long eq_idx){
+        EquipmentDomain equipmentDomain = equipmentRepo.findById(eq_idx).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. eq_idx="+eq_idx));
+        return new EquipmentResponseDto(equipmentDomain);
+    }
 }
