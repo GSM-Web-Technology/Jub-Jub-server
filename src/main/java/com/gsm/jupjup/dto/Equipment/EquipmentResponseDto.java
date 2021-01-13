b3337667ea738686bb323efa793b7149721ac529
siwony_ <siwon103305@gmail.com>
2021-01-13 16:51:46 +0900

    [add]Equipment Response DTO finish

--- src/main/java/com/gsm/jupjup/dto/Equipment/EquipmentResponseDto.java
+++ src/main/java/com/gsm/jupjup/dto/Equipment/EquipmentResponseDto.java
@@ -1,2 +1,24 @@
-package com.gsm.jupjup.dto.Equipment;public class EquipmentResponseDto {
+package com.gsm.jupjup.dto.Equipment;
+
+import com.gsm.jupjup.domain.EquipmentDomain;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EquipmentResponseDto {
+    private Long eq_Idx;
+    private String name;
+    private byte[] img_equipment;
+    private String content;
+    private int count;
+
+    public EquipmentResponseDto(EquipmentDomain equipmentDomain){
+        this.eq_Idx = equipmentDomain.getEq_Idx();
+        this.name = equipmentDomain.getName();
+        this.img_equipment = equipmentDomain.getImg_equipment();
+        this.content = equipmentDomain.getContent();
+        this.count = equipmentDomain.getCount();
+    }
 }
