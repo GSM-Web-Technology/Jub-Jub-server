05f1d546d61e75b7bd059ada1ce332cfdd6a6e67
johnjihwan <s20062@gsm.hs.kr>
2021-01-14 20:25:50 +0900

    [UPDATE]SpecService edit..

--- src/main/java/com/gsm/jupjup/dao/LaptopSpecRepository.java
+++ src/main/java/com/gsm/jupjup/dao/LaptopSpecRepository.java
@@ -3,4 +3,8 @@ package com.gsm.jupjup.dao;
 import com.gsm.jupjup.domain.LaptopSpecDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LaptopSpecRepository extends JpaRepository<LaptopSpecDomain, Long> { }
+import java.util.Optional;
+
+public interface LaptopSpecRepository extends JpaRepository<LaptopSpecDomain, Long> {
+    Optional<Object> findBySpec_Idx(String laptopSpec_idx);
+}
--- src/main/java/com/gsm/jupjup/service/LaptopSpecService.java
+++ src/main/java/com/gsm/jupjup/service/LaptopSpecService.java
@@ -1,9 +1,16 @@
 package com.gsm.jupjup.service;
 
 import com.gsm.jupjup.dao.LaptopSpecRepository;
+import com.gsm.jupjup.domain.LaptopSpecDomain;
+import com.gsm.jupjup.domain.TestDomain;
+import com.gsm.jupjup.dto.test.LaptopSaveRequestDto;
+import com.gsm.jupjup.dto.test.LaptopSpecSaveRequestDto;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Temporal;
 
 @Service
 public class LaptopSpecService {
@@ -12,6 +19,18 @@ public class LaptopSpecService {
     @Autowired
     private LaptopSpecRepository laptopSpecRepo;
 
+    // Create.
     @Transactional
+    public Long save(LaptopSpecSaveRequestDto laptopSpecSaveRequestDto){
+        return laptopSpecRepo.save(laptopSpecSaveRequestDto.toEntity()).getSpec_Idx();
+    }
+
+    //Update.
+    @Transactional
+    public String update(String laptopSpec_Idx, LaptopSpecSaveRequestDto laptopSpecSaveRequestDto){
+        LaptopSpecDomain laptopSpecDomain = (LaptopSpecDomain) laptopSpecRepo.findBySpec_Idx(laptopSpec_Idx).orElseThrow(() -> new IllegalIdentifierException("해당 사양의 노트북이 없습니다.:"+laptopSpec_Idx));
+        laptopSpecDomain.update(laptopSpecSaveRequestDto.getCPU(), laptopSpecSaveRequestDto.getGPU(), laptopSpecSaveRequestDto.getRAM(), laptopSpecSaveRequestDto.getSSD(), laptopSpecSaveRequestDto.getHDD());
+        return laptopSpec_Idx;
+    }
 
 }
