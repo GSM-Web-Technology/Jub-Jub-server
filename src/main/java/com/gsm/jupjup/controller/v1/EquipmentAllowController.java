4493862f0a91d0a0eab14240e821d3f0efa9e524
siwony_ <siwon103305@gmail.com>
2021-01-16 17:09:56 +0900

    [ADD]controller CRD finish

--- src/main/java/com/gsm/jupjup/controller/v1/EquipmentAllowController.java
+++ src/main/java/com/gsm/jupjup/controller/v1/EquipmentAllowController.java
@@ -1,2 +1,36 @@
-package com.gsm.jupjup.controller.v1;public class EquipmentAllowController {
+package com.gsm.jupjup.controller.v1;
+
+import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowRequestDto;
+import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowResponseDto;
+import com.gsm.jupjup.service.EquipmentAllowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EquipmentAllowController {
+
+    @Autowired
+    private EquipmentAllowService equipmentAllowService;
+
+    @PostMapping("/equipmentAllow")
+    public Long save(@RequestBody EquipmentAllowRequestDto equipmentAllowRequestDto){
+        return equipmentAllowService.save(equipmentAllowRequestDto);
+    }
+
+    @GetMapping("/equipmentAllow/{eqa_idx}")
+    public EquipmentAllowResponseDto equipmentAllowResponseDto(@PathVariable Long eqa_idx){
+        return equipmentAllowService.findById(eqa_idx);
+    }
+
+//    @PutMapping("/equipmentAllow/{epa_idx}")
+//    public Long update(@PathVariable Long epa_idx, @RequestBody EquipmentAllowRequestDto equipmentAllowRequestDto){
+//        return equipmentAllowService.update(epa_idx, equipmentAllowRequestDto);
+//    }
+
+    @PostMapping("/equipmentAllow/{eqa_idx}")
+    public void delete(@PathVariable Long eqa_idx){
+        equipmentAllowService.deleteById(eqa_idx);
+    }
+
 }
