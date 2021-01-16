d340e86dc4bedcba15b705db253b19742518c3d5
siwony_ <siwon103305@gmail.com>
2021-01-16 17:12:43 +0900

    [ADD]EquipmentAllowRequestDto finish

--- src/main/java/com/gsm/jupjup/dto/EquipmentAllow/EquipmentAllowRequestDto.java
+++ src/main/java/com/gsm/jupjup/dto/EquipmentAllow/EquipmentAllowRequestDto.java
@@ -2,7 +2,6 @@ package com.gsm.jupjup.dto.EquipmentAllow;
 
 import com.gsm.jupjup.domain.EquipmentAllowDomain;
 
-import com.gsm.jupjup.domain.EquipmentEnum;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,20 +12,17 @@ public class EquipmentAllowRequestDto {
 
     private int amount;
     private String reason;
-    private EquipmentEnum equipmentEnum;
 
     @Builder
-    public EquipmentAllowRequestDto(int amount, String reason, EquipmentEnum equipmentEnum){
+    public EquipmentAllowRequestDto(int amount, String reason){
         this.amount = amount;
         this.reason = reason;
-        this.equipmentEnum = equipmentEnum;
     }
 
     public EquipmentAllowDomain toEntity(){
         return EquipmentAllowDomain.builder()
                 .amount(this.amount)
                 .reason(this.reason)
-                .equipmentEnum(this.equipmentEnum)
                 .build();
     }
 
