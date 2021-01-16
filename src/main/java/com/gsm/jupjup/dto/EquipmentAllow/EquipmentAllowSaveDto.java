88dd3ae8857f9542501d0d006bbe34b3cf49e3b1
siwony_ <siwon103305@gmail.com>
2021-01-16 21:34:52 +0900

    [ADD]EquipmentAllowSaveDto

--- src/main/java/com/gsm/jupjup/dto/EquipmentAllow/EquipmentAllowSaveDto.java
+++ src/main/java/com/gsm/jupjup/dto/EquipmentAllow/EquipmentAllowSaveDto.java
@@ -1,2 +1,27 @@
-package com.gsm.jupjup.dto.EquipmentAllow;public class EquipmentAllowSaveDto {
+package com.gsm.jupjup.dto.EquipmentAllow;
+
+import com.gsm.jupjup.domain.EquipmentAllowDomain;
+import com.gsm.jupjup.domain.EquipmentDomain;
+import lombok.Builder;
+
+public class EquipmentAllowSaveDto {
+    private int amount;
+    private String reason;
+    private EquipmentDomain equipmentDomain;
+
+    @Builder
+    public EquipmentAllowSaveDto(EquipmentAllowRequestDto equipmentAllowRequestDto, EquipmentDomain equipmentDomain){
+        this.amount = equipmentAllowRequestDto.getAmount();
+        this.reason = equipmentAllowRequestDto.getReason();
+        this.equipmentDomain = equipmentDomain;
+    }
+
+    public EquipmentAllowDomain toEntity(){
+        return EquipmentAllowDomain.builder()
+                .amount(this.amount)
+                .reason(this.reason)
+                .equipmentDomain(this.equipmentDomain)
+                .build();
+    }
+
 }
