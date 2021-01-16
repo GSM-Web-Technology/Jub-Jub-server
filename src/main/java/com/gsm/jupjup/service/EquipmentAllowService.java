5d24a76029677cbfa4183a31a06753ff78ab5726
siwony_ <siwon103305@gmail.com>
2021-01-16 17:09:15 +0900

    [ADD]equipmentAllow CRUD finish

--- src/main/java/com/gsm/jupjup/service/EquipmentAllowService.java
+++ src/main/java/com/gsm/jupjup/service/EquipmentAllowService.java
@@ -1,4 +1,40 @@
 package com.gsm.jupjup.service;
 
+import com.gsm.jupjup.dao.EquipmentAllowRepository;
+import com.gsm.jupjup.domain.EquipmentAllowDomain;
+import com.gsm.jupjup.dto.Equipment.EquipmentRequestDto;
+import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowRequestDto;
+import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class EquipmentAllowService {
+    @Autowired
+    EquipmentAllowRepository equipmentAllowRepo;
+
+    @Transactional
+    public Long save(EquipmentAllowRequestDto equipmentAllowRequestDto){
+        return equipmentAllowRepo.save(equipmentAllowRequestDto.toEntity()).getEqa_Idx();
+    }
+
+    @Transactional
+    public Long update(Long eqa_idx, EquipmentAllowRequestDto equipmentRequestDto){
+        EquipmentAllowDomain equipmentAllowDomain = equipmentAllowRepo.findById(eqa_idx).orElseThrow(() -> new IllegalArgumentException("해당 신청은 없습니다. eqa_idx"+eqa_idx));
+        equipmentAllowDomain.update(equipmentRequestDto.getAmount(), equipmentRequestDto.getReason());
+        return equipmentAllowDomain.getEqa_Idx();
+    }
+
+    @Transactional
+    public void deleteById(long eqa_idx){
+        EquipmentAllowDomain equipmentAllowDomain = equipmentAllowRepo.findById(eqa_idx).orElseThrow(() -> new IllegalArgumentException("해당 신청은 없습니다. eqa_idx"+eqa_idx));
+        equipmentAllowRepo.delete(equipmentAllowDomain);
+    }
+
+    @Transactional(readOnly = true)
+    public EquipmentAllowResponseDto findById(Long eqa_idx){
+        EquipmentAllowDomain equipmentAllowDomain = equipmentAllowRepo.findById(eqa_idx).orElseThrow(() -> new IllegalStateException("해당 신청은 없습니다. eqa_idx="+eqa_idx));
+        return new EquipmentAllowResponseDto(equipmentAllowDomain);
+    }
 }
