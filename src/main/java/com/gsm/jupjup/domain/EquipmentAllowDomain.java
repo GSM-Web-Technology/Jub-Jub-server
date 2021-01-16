463821701bb894172ed6a46b82e2c77825d42401
siwony_ <siwon103305@gmail.com>
2021-01-16 22:02:46 +0900

    [FIX]Dont auto increment form 1

--- src/main/java/com/gsm/jupjup/domain/EquipmentAllowDomain.java
+++ src/main/java/com/gsm/jupjup/domain/EquipmentAllowDomain.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class EquipmentAllowDomain extends BaseTimeEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eqa_Idx;
 
     @Column
