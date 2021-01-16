6d7943bab7fa82c3c7a32c641669915eb996ae42
siwony_ <siwon103305@gmail.com>
2021-01-16 22:00:27 +0900

    [UPDATE]implement equipmentAllow save logic amount uncount at EquipmentAllowService

--- src/main/java/com/gsm/jupjup/service/EquipmentAllowService.java
+++ src/main/java/com/gsm/jupjup/service/EquipmentAllowService.java
@@ -1,10 +1,12 @@
 package com.gsm.jupjup.service;
 
 import com.gsm.jupjup.dao.EquipmentAllowRepository;
+import com.gsm.jupjup.dao.EquipmentRepository;
 import com.gsm.jupjup.domain.EquipmentAllowDomain;
-import com.gsm.jupjup.dto.Equipment.EquipmentRequestDto;
+import com.gsm.jupjup.domain.EquipmentDomain;
 import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowRequestDto;
 import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowResponseDto;
+import com.gsm.jupjup.dto.EquipmentAllow.EquipmentAllowSaveDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +15,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class EquipmentAllowService {
     @Autowired
     EquipmentAllowRepository equipmentAllowRepo;
+    @Autowired
+    EquipmentRepository equipmentRepo;
 
     @Transactional
     public Long save(EquipmentAllowRequestDto equipmentAllowRequestDto){
         return equipmentAllowRepo.save(equipmentAllowRequestDto.toEntity()).getEqa_Idx();
+    }
+
+    @Transactional
+    public void saveEquipmentAllowData(String equipmentName , EquipmentAllowRequestDto equipmentAllowRequestDto){
+        EquipmentDomain equipmentDomain = equipmentRepo.findByName(equipmentName).orElseThrow(() -> new IllegalArgumentException("해당 기자제는 없습니다."));
+        EquipmentAllowSaveDto equipmentAllowSaveDto =
+                EquipmentAllowSaveDto.builder().
+                                    equipmentAllowRequestDto(equipmentAllowRequestDto).
+                                    equipmentDomain(equipmentDomain)
+                                    .build();
+        EquipmentAllowDomain equipmentAllowDomain = equipmentAllowSaveDto.toEntity();
+        equipmentAllowRepo.save(equipmentAllowDomain);
     }
 
     @Transactional
@@ -27,7 +43,7 @@ public class EquipmentAllowService {
     }
 
     @Transactional
-    public void deleteById(long eqa_idx){
+    public void deleteById(Long eqa_idx){
         EquipmentAllowDomain equipmentAllowDomain = equipmentAllowRepo.findById(eqa_idx).orElseThrow(() -> new IllegalArgumentException("해당 신청은 없습니다. eqa_idx"+eqa_idx));
         equipmentAllowRepo.delete(equipmentAllowDomain);
     }
@@ -37,4 +53,6 @@ public class EquipmentAllowService {
         EquipmentAllowDomain equipmentAllowDomain = equipmentAllowRepo.findById(eqa_idx).orElseThrow(() -> new IllegalStateException("해당 신청은 없습니다. eqa_idx="+eqa_idx));
         return new EquipmentAllowResponseDto(equipmentAllowDomain);
     }
+
+
 }
