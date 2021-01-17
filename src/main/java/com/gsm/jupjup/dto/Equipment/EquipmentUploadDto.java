dbfeefdf0dc69025ab9a6a0673bc2692206e2c11
siwony_ <siwon103305@gmail.com>
2021-01-17 19:58:37 +0900

    [UPDATE]remove white space

--- src/main/java/com/gsm/jupjup/dto/Equipment/EquipmentUploadDto.java
+++ src/main/java/com/gsm/jupjup/dto/Equipment/EquipmentUploadDto.java
@@ -1,2 +1,24 @@
-package com.gsm.jupjup.dto.Equipment;public class EquipmentUploadDto {
+package com.gsm.jupjup.dto.Equipment;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Getter
+@NoArgsConstructor
+public class EquipmentUploadDto {
+    private MultipartFile img_equipment;
+    private String name;
+    private String content;
+    private int count;
+
+    @Builder
+    public EquipmentUploadDto(MultipartFile img_equipment, String name, String content,  int count){
+        this.img_equipment = img_equipment;
+        this.name = name;
+        this.content = content;
+        this.count = count;
+    }
 }
--- src/main/java/com/gsm/jupjup/service/EquipmentAllowService.java
+++ src/main/java/com/gsm/jupjup/service/EquipmentAllowService.java
@@ -17,7 +17,6 @@ public class EquipmentAllowService {
     EquipmentAllowRepository equipmentAllowRepo;
     @Autowired
     EquipmentRepository equipmentRepo;
-
     @Autowired
     EquipmentService equipmentService;
 
