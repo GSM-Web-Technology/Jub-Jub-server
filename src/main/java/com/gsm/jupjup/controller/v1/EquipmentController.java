1a53b355539231b411581bf8fab1ff70b5f1ecf5
siwony_ <siwon103305@gmail.com>
2021-01-17 19:34:00 +0900

    [UPDATE]equipment save controller finish at EquipmentController

--- src/main/java/com/gsm/jupjup/controller/v1/EquipmentController.java
+++ src/main/java/com/gsm/jupjup/controller/v1/EquipmentController.java
@@ -3,9 +3,13 @@ package com.gsm.jupjup.controller.v1;
 
 import com.gsm.jupjup.dto.Equipment.EquipmentRequestDto;
 import com.gsm.jupjup.dto.Equipment.EquipmentResponseDto;
+import com.gsm.jupjup.dto.Equipment.EquipmentUploadDto;
 import com.gsm.jupjup.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -14,9 +18,17 @@ public class EquipmentController {
     @Autowired
     private EquipmentService equipmentService;
 
-    @PostMapping("/equipment")
-    public Long save(@RequestBody EquipmentRequestDto equipmentRequestDto){
-        return equipmentService.save(equipmentRequestDto);
+    @PostMapping("/equimpent")
+    public void save(@RequestParam("img_equipment") MultipartFile img_equipment, @RequestParam String name, @RequestParam String content, @RequestParam int count) throws IOException{
+        EquipmentUploadDto equipmentUploadDto
+                = EquipmentUploadDto.builder()
+                .img_equipment(img_equipment)
+                .name(name)
+                .content(content)
+                .count(count)
+                .build();
+
+        equipmentService.save(equipmentUploadDto);
     }
 
     @GetMapping("/equipment/{name}")
