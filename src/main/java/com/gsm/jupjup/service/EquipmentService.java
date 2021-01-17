2557ab86b0a766e6d39e96455abf347aa03b2a3e
siwony_ <siwon103305@gmail.com>
2021-01-18 03:05:08 +0900

    [UPDATE]Equipment part code refactoring

--- src/main/java/com/gsm/jupjup/service/EquipmentService.java
+++ src/main/java/com/gsm/jupjup/service/EquipmentService.java
@@ -2,9 +2,9 @@ package com.gsm.jupjup.service;
 
 import com.gsm.jupjup.dao.EquipmentRepository;
 import com.gsm.jupjup.domain.EquipmentDomain;
-import com.gsm.jupjup.dto.Equipment.EquipmentRequestDto;
+import com.gsm.jupjup.dto.Equipment.EquipmentReqDto;
 import com.gsm.jupjup.dto.Equipment.EquipmentUploadDto;
-import com.gsm.jupjup.dto.Equipment.EquipmentResponseDto;
+import com.gsm.jupjup.dto.Equipment.EquipmentResDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,30 +21,30 @@ public class EquipmentService {
     @Transactional
     public void save(EquipmentUploadDto equipmentUploadDto) throws IOException {
         MultipartFile img_equipment = equipmentUploadDto.getImg_equipment();
-        byte[] conversionImg_equipment = img_equipment.getBytes();
+        byte[] img_equipmentToByte = img_equipment.getBytes();
 
-        EquipmentRequestDto equipmentRequestDto =
-                EquipmentRequestDto.builder()
-                        .img_equipment(conversionImg_equipment)
+        EquipmentReqDto equipmentReqDto =
+                EquipmentReqDto.builder()
+                        .img_equipment(img_equipmentToByte)
                         .content(equipmentUploadDto.getContent())
                         .name(equipmentUploadDto.getName())
                         .count(equipmentUploadDto.getCount())
                         .build();
 
-        EquipmentDomain equipmentDomain = equipmentRequestDto.toEntity();
+        EquipmentDomain equipmentDomain = equipmentReqDto.toEntity();
         equipmentRepo.save(equipmentDomain);
     }
 
     @Transactional
-    public Long update(String name, EquipmentRequestDto equipmentRequestDto){
-        EquipmentDomain equipmentDomain = equipmentRepo.findByName(name).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. name="+name));
-        equipmentDomain.update(equipmentRequestDto.getName(), equipmentRequestDto.getImg_equipment(), equipmentRequestDto.getContent(), equipmentRequestDto.getCount());
+    public Long update(String name, EquipmentReqDto equipmentReqDto){
+        EquipmentDomain equipmentDomain = equipmentFindByName(equipmentRepo, name);
+        equipmentDomain.update(equipmentReqDto);
         return equipmentDomain.getEq_Idx();
     };
 
     @Transactional
     public String updateAmount(String name, int count){
-        EquipmentDomain equipmentDomain = equipmentRepo.findByName(name).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. name="+name));
+        EquipmentDomain equipmentDomain = equipmentFindByName(equipmentRepo, name);
         equipmentDomain.updateCount(count);
         equipmentRepo.save(equipmentDomain);
         return equipmentDomain.getName();
@@ -52,21 +52,26 @@ public class EquipmentService {
 
     @Transactional
     public void deleteByName(String name){
-        EquipmentDomain equipmentDomain = equipmentRepo.findByName(name).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. name="+name));
+        EquipmentDomain equipmentDomain = equipmentFindByName(equipmentRepo, name);
         equipmentRepo.delete(equipmentDomain);
     }
 
     @Transactional
     public byte[] findByNameGetEquipment_img(String name){
-        return equipmentRepo.findByName(name).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. name="+name)).getImg_equipment();
+        return equipmentFindByName(equipmentRepo, name).getImg_equipment();
     }
 
     @Transactional(readOnly = true)
-    public EquipmentResponseDto findByName(String name){
-        EquipmentDomain equipmentDomain = equipmentRepo.findByName(name).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. name="+name));
-        return new EquipmentResponseDto(equipmentDomain);
+    public EquipmentResDto findByName(String name){
+        EquipmentDomain equipmentDomain = equipmentFindByName(equipmentRepo, name);
+        return new EquipmentResDto(equipmentDomain);
     }
 
+    //Equipment를 name으로 찾고 Entity만드는 매서드
+    @Transactional
+    public EquipmentDomain equipmentFindByName(EquipmentRepository equipmentRepo, String name){
+        return equipmentFindByName(equipmentRepo, name);
+    }
 //    @Transactional(readOnly = true)
 //    public BufferedImage findByName(String name) throws IOException {
 //        EquipmentDomain equipmentDomain = equipmentRepo.findByName(name).orElseThrow(() -> new IllegalArgumentException("해당 기자재는 없습니다. name="+name));
