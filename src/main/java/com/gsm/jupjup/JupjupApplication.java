11dc9f4197ae75b04cc709c3a9172817f2a597b4
siwony_ <siwon103305@gmail.com>
2021-01-18 10:15:38 +0900

    Update JupjupApplication.java

--- src/main/java/com/gsm/jupjup/JupjupApplication.java
+++ src/main/java/com/gsm/jupjup/JupjupApplication.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class JupjupApplication implements CommandLineRunner{
+public class JupjupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JupjupApplication.class, args);
